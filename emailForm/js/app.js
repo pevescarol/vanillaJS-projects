2ff654a16c00db07effff7f819e67748c05ff285
pevescarol <carol.peves@gmail.com>
2023-03-14 12:04:19 -0300

    se compruebaEmail y se hace cambios en boton enviar

--- emailForm/js/app.js
+++ emailForm/js/app.js
@@ -1,15 +1,21 @@
 // 
+const emailMsg={
+    email: '',
+    asunto: '',
+    mensaje: ''
+}
 
 const email = document.querySelector("#email")
 const asunto = document.querySelector("#asunto")
 const mensaje = document.querySelector("#mensaje")
 const formulario = document.querySelector("#form")
+const btnSubmit = document.querySelector("#form button[type=\"submit\"]")
 
 // Asignar eventos
 
-email.addEventListener("blur", validar)
-asunto.addEventListener("blur", validar)
-mensaje.addEventListener("blur", validar)
+email.addEventListener("input", validar)
+asunto.addEventListener("input", validar)
+mensaje.addEventListener("input", validar)
 
 // validar entrada al inut
 
@@ -17,15 +23,25 @@ function validar(e){
     //console.log(e.target.parentElement)
     if(e.target.value.trim() === ""){
         mostrarAlerta(`El campo ${e.target.id} es obligatorio`, e.target.parentElement)
+        emailMsg[e.target.name] = ''
+        comprobarEmail()
         return  //para detenet el cod
     }
 
     if(e.target.id === "email" && !validarEmail(e.target.value)) {
         mostrarAlerta("El email no es válido", e.target.parentElement)
+        emailMsg[e.target.name] = '' // lo reiniciamos
+        comprobarEmail()  // se comprueba el mail
         return
     }
     
     limpiarAlerta(e.target.parentElement)
+
+    // asignar los valores
+    emailMsg[e.target.name] = e.target.value.trim().toLowerCase()
+    //console.log(emailMsg)
+
+    comprobarEmail()
 }
 
 // generar alerta en HTML
@@ -57,4 +73,17 @@ function validarEmail(email) {
     const regex = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/
     const resultado = regex.test(email)
     return resultado
+}
+
+function comprobarEmail() {
+    // chequea que todos los campos deben estar llenos
+    if(Object.values(emailMsg).includes('')){
+        btnSubmit.classList.add("enviar")
+        btnSubmit.disabled = true
+        return
+    }
+
+    btnSubmit.classList.remove("enviar")
+    btnSubmit.disabled = false
+    
 }
